9c43bec3af73213a3a901d91b2abbc231d150c2d
gobai <599500688@qq.com>
2026-04-26 19:37:10 +0800

    Refine transfer cancel path and timeout checks

--- pkg/transfer.go
+++ pkg/transfer.go
@@ -2,47 +2,15 @@ package pkg
 
 import (
 	"context"
+	"errors"
 	"io"
 	"log"
 	"net"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 )
-
-type TransferResult struct {
-	N   int
-	Err error
-}
-
-func read(ctx context.Context, conn net.Conn, buf []byte) TransferResult {
-	ch := make(chan TransferResult)
-	go func() {
-		// ignore if context is done
-		n, err := conn.Read(buf)
-		ch <- TransferResult{n, err}
-	}()
-	select {
-	case <-ctx.Done():
-		return TransferResult{0, ctx.Err()}
-	case res := <-ch:
-		return res
-	}
-}
-
-func write(ctx context.Context, conn net.Conn, buf []byte) TransferResult {
-	ch := make(chan TransferResult)
-	go func() {
-		// ignore if context is done
-		n, err := conn.Write(buf)
-		ch <- TransferResult{n, err}
-	}()
-	select {
-	case <-ctx.Done():
-		return TransferResult{0, ctx.Err()}
-	case res := <-ch:
-		return res
-	}
-}
 
 func Transfer(ctx context.Context, destination, source net.Conn) {
 	defer func() {
@@ -51,28 +19,20 @@ func Transfer(ctx context.Context, destination, source net.Conn) {
 		atomic.AddInt64(&ActiveConnections, -1)
 	}()
 
+	stopCancelHook := context.AfterFunc(ctx, func() {
+		// Unblock io.CopyBuffer when graceful shutdown starts.
+		_ = source.SetReadDeadline(time.Now())
+		_ = destination.SetWriteDeadline(time.Now())
+	})
+	defer stopCancelHook()
+
 	buf := make([]byte, 32*1024)
-	for {
-		readRes := read(ctx, source, buf)
-		if readRes.N > 0 {
-			writeRes := write(ctx, destination, buf[0:readRes.N])
-			// TODO: check nw < 0 or nr < nw, record transferred bytes
-			// ignore errors
-			if writeRes.Err != nil {
-				return
-			}
-			if readRes.N != writeRes.N {
-				return
-			}
-		}
-		if readRes.Err != nil {
-			// TODO filter use of closed network connection error and EOF
-			if readRes.Err != io.EOF &&
-				!strings.Contains(readRes.Err.Error(), "use of closed network connection") &&
-				!strings.Contains(readRes.Err.Error(), "context canceled") {
-				log.Printf("Read error: %v", readRes.Err)
-			}
-			return
-		}
+	_, err := io.CopyBuffer(destination, source, buf)
+	if err != nil &&
+		err != io.EOF &&
+		!strings.Contains(err.Error(), "use of closed network connection") &&
+		!strings.Contains(err.Error(), "context canceled") &&
+		!(ctx.Err() != nil && errors.Is(err, os.ErrDeadlineExceeded)) {
+		log.Printf("Transfer error: %v", err)
 	}
 }
